71a93db5f7504e4f8797b26cb1e3e02a19857073
Tzahi Levi <tazachil@gmail.com>
2024-07-20 15:55:07 +0300

    #120 - fixed unnecessary imports

--- frontend/src/app/shared/components/skills-view/skills-view.component.ts
+++ frontend/src/app/shared/components/skills-view/skills-view.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { Skill } from '../../models/skill.model';
-import { sample_skills } from '../../../../data';
 
 @Component({
   selector: 'app-skills-view',
